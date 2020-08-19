a1050c2f5e426b742b5cb9f6d179814ec34caaef
kyle-mcr <kylemcreynolds92@gmail.com>
2020-08-19 11:08:32 -0700

    added models

--- Angular-Bootstrap-with-Material-Design/src/app/app.module.ts
+++ Angular-Bootstrap-with-Material-Design/src/app/app.module.ts
@@ -1,25 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- import FormsModule.
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HttpService } from './http.service';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    FormsModule
+    MDBBootstrapModule.forRoot()
   ],
-  providers: [HttpService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- public/src/app/home/home.component.ts
+++ public/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, FormArray } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  
+cards = [
+    {
+      title: 'Expenses',
+      description: 'Do you have a monthly student payment?',
+      value: 0
+    },
+    {
+      title: 'Expenses',
+      description: 'Do you have a monthly car loan payment?',
+      value: 0
+    },
+    {
+      title: 'Expenses',
+      description: 'Do you have a monthly car insurance payment?',
+      value: 0
+    },
+    {
+      title: 'Expenses',
+      description: 'How much do you estimate you spend on gas for your car monthly?',
+      value: 0
+    },
+    {
+      title: 'Expenses',
+      description: 'How much do you estimate you spend per month for ',
+      value: 0
+    },
+    {
+      title: 'Expenses',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      value: 0
+    },
+    {
+      title: 'Expenses',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      value: 0
+    },
+    {
+      title: 'Expenses',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      value: 0
+    },
+    {
+      title: 'Expenses',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      value: 0
+    },
+  ];
+  numbersInForm: any = {};
+  newArry: any = [];
+  mortgageRateFound: any;
+  calculate: number;
+  newString: string = '';
+  constructor(
 
-  constructor() { }
+  ) { }
 
   ngOnInit() {
+
   }
 
+  getClicked() {
+      this.mortgageRateFound = [];
+      for (let key in this.numbersInForm) {
+          if (this.numbersInForm[key] == true) {
+              this.newArry.push(key)
+          }
+      }
+    }
 }
