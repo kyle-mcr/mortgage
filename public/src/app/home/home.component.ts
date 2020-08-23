8c54c5e66e5bbe0a8f999991a44644fddfb3e2f5
kyle-mcr <kylemcreynolds92@gmail.com>
2020-08-22 19:40:13 -0700

    progress ribbon

--- public/src/app/home/home.component.ts
+++ public/src/app/home/home.component.ts
@@ -10,59 +10,88 @@ declare var jQuery: any;
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  selectedIndex = -1;
+  showContent(evt, index) {
+    this.selectedIndex = index;
+  }
   cards = [
     {
-      title: "Expenses",
-      description: "Do you have a monthly student payment?",
-      value: 0,
+      description: "Do you have a monthly student loan payment?",
     },
     {
-      title: "Expenses",
       description: "Do you have a monthly car loan payment?",
-      value: 0,
     },
     {
-      title: "Expenses",
       description: "Do you have a monthly car insurance payment?",
-      value: 0,
     },
     {
-      title: "Expenses",
       description:
         "How much do you estimate you spend on gas for your car monthly?",
-      value: 0,
     },
     {
-      title: "Expenses",
-      description: "How much do you estimate you spend per month for ",
-      value: 0,
-    },
-    {
-      title: "Expenses",
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      value: 0,
+        "How much do you estimate you spend per month for car maintenance?",
     },
     {
-      title: "Expenses",
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      value: 0,
+        "Do you have any monthly health/dental expenses(including health insurance, medical bills, prescription drugs) ?",
     },
     {
-      title: "Expenses",
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      value: 0,
+        "How much do you estimate you spend on groceries and essential supplies per month",
     },
     {
-      title: "Expenses",
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card content",
-      value: 0,
+        "Do you pay for a fitness membership?",
     },
+    {
+      description:
+        "Do you pay for any streaming services?",
+    },
+    {
+      description:
+        "Do you pay a monthly phone bill?",
+    },
+    {
+      description:
+        "How much do you estimate you spend on dining out per month?",
+    },
+    {
+      description:
+        "Do you pay for child care or child support?",
+    },
+    {
+      description:
+        "Estimated monthly cost for reacreation and entertainment?",
+    },
+    {
+      description:
+        "Any other monthly expenses that aren't accounted for please add them here.",
+    },
+    {
+      description:
+        "What is your monthyl income per month after taxes?",
+    },
+    {
+      description:
+        "What is your total household income after taxes?",
+    },
+    {
+      description:
+        "Do you have income from any sources other than salary?",
+    },
+    {
+      description:
+        "Do you recieve any other monthly benefits(social security benefits, disability income, etc) ?",
+    },
+    {
+      description:
+        "Do you have any other sources of perpetual monthly income?",
+    },
+  
   ];
   numbersInForm: any = {};
+  newObj = {};
   newArry: any = [];
   mortgageRateFound: any;
   calculate: number;
@@ -73,19 +102,20 @@ export class HomeComponent implements OnInit {
     (function ($) {
       $(document).ready(function () {
         $("button").click(function () {
-          $("#jumbo, #fadeButton").hide(1000);
-          $("form").show(900);
+          $("#jumbo, #fadeButton").hide(200, "linear");
+          $("form").show(200, "linear");
         });
       });
     })(jQuery);
   }
 
   getClicked() {
+    this.newObj = {};
     this.mortgageRateFound = [];
-    for (let key in this.numbersInForm) {
-      if (this.numbersInForm[key] == true) {
-        this.newArry.push(key);
-      }
-    }
+    const out = Object.assign(
+      {},
+      ...Object.values(this.numbersInForm).map((v, i) => ({ [i + 1]: v }))
+    );
+    console.log(out);
   }
 }
